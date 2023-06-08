fbc1c1af900c28d69d4143ed15ee835c1b857989
Tony-Stark <sz.unsc@gmail.com>
2023-06-08 16:07:05 +0300

    TTS

--- recorder.py
+++ recorder.py
@@ -21,7 +21,7 @@ def record(event):
     sample_format = pyaudio.paInt16  # 16 bits per sample
     channels = 2
     fs = 44100  # Record at 44100 samples per second
-    seconds = 3
+    seconds = 10
     filename = "output.wav"
 
     p = pyaudio.PyAudio()  # Create an interface to PortAudio
